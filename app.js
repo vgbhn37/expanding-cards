87abca9287300167db59049638de50c53e947db7
임지훈 <vgbhn@naver.com>
2023-02-16 10:27:34 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// alert('테스트');
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+ panel.addEventListener('click', () => {
+  removeActive() 
+  panel.classList.add('active')
+ })
+} )
+
+/* 액티브 클래스 제거 */
+function removeActive() {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
